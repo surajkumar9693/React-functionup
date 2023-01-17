a6be9c8d9016e8d804f9c8c671c912d7b18506d1
suraj <surajkumargt7262@gmail.com>
2023-01-17 20:18:32 +0530

    create

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home-header'>
+       
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
